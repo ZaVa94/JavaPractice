1df054bb6f198ffcf2b70e2f9a847dc9c8287e87
Zachary Vargas <Zachary Vargas>
2017-05-15 22:16:07 -0600

    Added a swing demo

---
+++ SwingFC/src/SwingFC.java
@@ -0,0 +1,99 @@
+/**
+ * Created by Zach on 5/15/2017.
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+public class SwingFC implements ActionListener {
+
+    JTextField jtfFirst;
+    JTextField jtfSecond;
+
+    JButton jbtnComp;
+
+    JLabel jlabFirst, jlabSecond;
+    JLabel jlabResult;
+
+    SwingFC()
+    {
+        JFrame jfrm = new JFrame("Compare Files");
+
+        jfrm.setLayout(new FlowLayout());
+
+        jfrm.setSize(200, 190);
+
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jtfFirst = new JTextField(14);
+        jtfSecond = new JTextField(14);
+
+        jtfFirst.setActionCommand("fileA");
+        jtfSecond.setActionCommand("fileB");
+
+        JButton jbtnComp = new JButton("Compare");
+
+        jbtnComp.addActionListener(this);
+
+        jlabFirst = new JLabel("First file: ");
+        jlabSecond = new JLabel("Second file: ");
+        jlabResult = new JLabel("");
+
+        jfrm.add(jlabFirst);
+        jfrm.add(jtfFirst);
+        jfrm.add(jlabSecond);
+        jfrm.add(jtfSecond);
+        jfrm.add(jbtnComp);
+        jfrm.add(jlabResult);
+
+        jfrm.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int i =0, j=0;
+
+        if (jtfFirst.getText().equals(""))
+        {
+            jlabResult.setText("First file name missing.");
+            return;
+        }
+        if (jtfSecond.getText().equals(""))
+        {
+            jlabResult.setText("Second file name missing.");
+            return;
+        }
+
+        try (FileInputStream f1 = new FileInputStream(jtfFirst.getText());
+        FileInputStream f2 = new FileInputStream(jtfSecond.getText())) {
+
+            do {
+                i = f1.read();
+                j = f2.read();
+                if (i != j) break;
+            } while (i != -1 && j != -1);
+
+            if (i != j)
+            {
+                jlabResult.setText("Files are not the same");
+            }
+            else
+            {
+                jlabResult.setText("Files compare equal.");
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SwingFC();
+            }
+        });
+    }
+}
